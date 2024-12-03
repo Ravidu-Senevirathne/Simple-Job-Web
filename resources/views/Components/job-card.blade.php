572a01121c692cc01e6a124be7f00d7986c0afef
ravidu_senevirathne <senevirathneravidu@gmail.com>
2024-12-03 20:41:21 +0530

    third commit created new components and added them

--- resources/views/Components/job-card.blade.php
+++ resources/views/Components/job-card.blade.php
@@ -10,9 +10,9 @@
     <div  class="flex justify-between items-center mt-auto">
 
         <div>
-            <a href="#" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
-            <a href="#" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
-            <a href="#" class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
+          <x-tag>Tag</x-tag>
+          <x-tag>Tag</x-tag>
+          <x-tag>Tag</x-tag>
             
         </div>
         <img src="https://placehold.it/42/42" alt="" class="rounded-xl">
