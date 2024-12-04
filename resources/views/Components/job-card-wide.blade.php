3f03253de10c25b29a8375797fbf96cfdac00d28
ravidu_senevirathne <senevirathneravidu@gmail.com>
2024-12-04 23:04:48 +0530

    created new components

--- resources/views/Components/job-card-wide.blade.php
+++ resources/views/Components/job-card-wide.blade.php
@@ -1,14 +1,14 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-800 group">
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-800 group transition-colors duration-1000">
 
    <div>
     <x-employer-logo/>
    </div>
 
 
-    <div class="flex-1 flex flex-col">
+    <div class="flex-1 flex flex-col cursor-pointer ">
         <a href="#" class="self-start text-sm text-gray-400">AgileWave</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">Video Producer</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 group transition-colors duration-1000">Video Producer</h3>
         <p class="text-sm text-gray-400 mt-auto">Full Time - From LKR60,000</p>
 
 
