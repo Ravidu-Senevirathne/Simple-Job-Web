1778bc86e690bb99e89669bdf01f1ce0e6d9b237
ravidu_senevirathne <senevirathneravidu@gmail.com>
2024-12-07 10:26:38 +0530

    Changed a bit

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,5 +1,17 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
-});
+use App\Models\Employer;
+use App\Models\Job;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+it("belongs to an Employer", function () {
+    $this->artisan('migrate');
+
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    expect($job->employer->is($employer))->toBeTrue();
+})->uses(RefreshDatabase::class);
