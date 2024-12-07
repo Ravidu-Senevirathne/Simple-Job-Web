19a30d8648747a52e6cb566d38b4c51affeac8aa
ravidu_senevirathne <senevirathneravidu@gmail.com>
2024-12-07 16:26:17 +0530

    file structure changed

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,9 +10,11 @@ class Job extends Model
 
     use HasFactory;
 
-    public function tag()
+    public function tag(string $name)
     {
-     
+     $tag = Tag::firstOrCreate(['name'=>$name]);
+
+     $this->tags()->attach($tag);
     }
     public function tags()
     {
