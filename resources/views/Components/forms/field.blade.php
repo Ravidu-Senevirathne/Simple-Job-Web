68b3a6fbe3b3a0474e745e62d201df977cf2a91b
ravidu_senevirathne <senevirathneravidu@gmail.com>
2024-12-08 16:33:53 +0530

    Created Registration Part

--- resources/views/Components/forms/field.blade.php
+++ resources/views/Components/forms/field.blade.php
@@ -1,13 +1,13 @@
-@props('label', 'name')
+@props(['label', 'name'])
 
 <div>
     @if($label)
-        <x-forms.label :$name :$label />
+        <x-forms.label :name="$name" :label="$label" />
     @endif
 
     <div class="mt-1">
         {{ $slot }}
 
-        <x-forms.error :error="$errors->first($name)"/>
+        <x-forms.error :error="$errors->first($name)" />
     </div>
 </div>
